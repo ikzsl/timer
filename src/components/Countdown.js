1528773973800e6f7aa31a0367ddcb7bbda39583
Muslim <ikzsl@yandex.ru>
2020-05-03 20:08:18 +0300

    test

---
+++ src/components/Countdown.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Countdown = () => {
+    return (
+        <p className={{color: "red"}}>I am Countdown</p>
+    )
+}
+
+export default Countdown;
---
+++ src/components/Timer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Timer = () => {
+    return (
+        <p>I am Timer</p>
+    )
+}
+
+export default Timer;
