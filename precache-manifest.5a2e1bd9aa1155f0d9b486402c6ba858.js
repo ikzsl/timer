e1c35d67fae9704c4a978d609e3072f951c6e6aa
Muslim <ikzsl@yandex.ru>
2020-05-04 20:29:21 +0300

    Updates

---
+++ precache-manifest.5a2e1bd9aa1155f0d9b486402c6ba858.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "871f8fc1a44c34cf101286125fcce7b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d474d86d47b98dbaf21a",
+    "url": "/static/css/main.22d33e75.chunk.css"
+  },
+  {
+    "revision": "7b50b5c96a230788ff8a",
+    "url": "/static/js/2.93de7ab3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.93de7ab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d474d86d47b98dbaf21a",
+    "url": "/static/js/main.9cf1e23e.chunk.js"
+  },
+  {
+    "revision": "5b485aa4f213e166d63e",
+    "url": "/static/js/runtime-main.75ec8814.js"
+  },
+  {
+    "revision": "d977555c196e297bc9ae8dc8e91dc616",
+    "url": "/static/media/logo.d977555c.png"
+  }
+]);
