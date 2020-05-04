8fa40e503cd0350fdd262cda24c0ad6cf3200c6b
Muslim <ikzsl@yandex.ru>
2020-05-04 20:49:02 +0300

    add gh-pages deploy

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
-const Countdown = () => <p className={{ color: 'red' }}>I am Countdown</p>;
+const Countdown = () => (
+  <p className="jdghfjhsdfg kjhkhkj k jhkjhkj kjhkjhkjhk kjhkj">I am Countdown</p>
+);
 
 export default Countdown;
